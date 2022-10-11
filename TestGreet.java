8155b7e6f8a6d2e17769fd90ca44cd8d85b45478
wulandnn <82815919+wulandnn@users.noreply.github.com>
2022-10-11 12:30:10 +0700

    Menambahkan codingan menggunakan bahasa Java

---
+++ TestGreet.java
@@ -0,0 +1,9 @@
+//
+//Sample "Welcome to Java Programming!" application
+//
+public class TestGreet {
+public static void main (String[] args){
+     Greeting hello = new Greeting();
+     hello.greet();
+  }
+}
